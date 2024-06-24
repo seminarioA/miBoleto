92afa1248ab94a53b4bc9811288db4a806e67fbc
ALEJANDRO VALENTINO SEMINARIO MEDINA <88020645+seminarioA@users.noreply.github.com>
2024-06-24 13:40:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// script.js
 $(document).ready(function() {
     function myFunction() {
         // Get the value of the input field with id="numb"
         let x = $("#numb").val();
+        // Set the text of the element with id="demo" to the value of the input field
         $("#demo").text(x);
     }
 
