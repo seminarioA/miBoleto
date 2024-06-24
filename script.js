de5332f4a59cd7de891384e22d3417adbc1ccd23
ALEJANDRO VALENTINO SEMINARIO MEDINA <88020645+seminarioA@users.noreply.github.com>
2024-06-23 19:21:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-function myFunction() {
-  // Get the value of the input field with id="numb"
-  let x = $("numb").value;
-  // If x is Not a Number or less than one or greater than 10
-  let text;
-  if (isNaN(x) || x < 1 || x > 10) {
-    text = "Input not valid";
-  } else {
-    text = "Input OK";
-  }
-  $("demo").innerHTML = text;
-}
+// script.js
+$(document).ready(function() {
+    function myFunction() {
+        // Get the value of the input field with id="numb"
+        let x = $("#numb").val();
+        // If x is Not a Number or less than one or greater than 10
+        let text;
+        if (isNaN(x) || x < 1 || x > 10) {
+            text = "Input not valid";
+        } else {
+            text = "Input OK";
+        }
+        $("#demo").text(text);
+    }
+
+    // Bind the function to the button click event
+    $("button").on("click", myFunction);
+});
