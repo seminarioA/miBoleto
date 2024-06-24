9e808fa9cbce3de822583caa7ffdff3ae6c69f96
ALEJANDRO VALENTINO SEMINARIO MEDINA <88020645+seminarioA@users.noreply.github.com>
2024-06-23 19:45:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,8 @@
 $(document).ready(function() {
     function myFunction() {
         // Get the value of the input field with id="numb"
-        let x = $("#numb").val();
-        // If x is Not a Number or less than one or greater than 10
-        let text;
-        if (isNaN(x) || x < 1 || x > 10) {
-            text = "Input not valid";
-        } else {
-            text = "Input OK";
-        }
-        $("#demo").text(text);
+        let x = $("#numb");
+        $("#demo").text(x);
     }
 
     // Bind the function to the button click event
